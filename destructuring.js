972343034662bde6c8c11a003bef430725ae5171
aknanta <aknantaa@gmail.com>
2022-06-22 02:02:00 +0700

    add some file

---
+++ destructuring.js
@@ -0,0 +1,39 @@
+//DESTRUCTURING ARRAY
+const person = ["Aknanta", "Akmal"]
+
+const [firstName, lastName] = person;
+
+console.info(firstName, lastName)
+
+//DESTRUCTURING OBJECT
+
+const orang = {
+    namaDepan: "Aknanta",
+    namaBelakang: "Akmal",
+    alamat: {
+        jalan: "parkit"
+    }
+}
+
+const {namaDepan, namaBelakang, alamat: {jalan}} = orang;
+
+console.info(namaBelakang, jalan)
+
+//DESTRUCTURING DI FUNCTION PARAMETER
+
+function displayPerson({jenengNgarep, jenengMburi,}){
+    console.info(jenengNgarep);
+    console.info(jenengMburi);
+}
+
+const human = {
+    jenengNgarep : "Aknanta",
+    jenengMburi: "Akmal"
+}
+displayPerson(human)
+
+function sum([first, second]){
+    return first + second
+}
+
+console.info([10 + 20])
